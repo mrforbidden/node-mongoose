f07ff3d5d7319736b9f8b50673d8a54e93758435
chuanxu7 <xuchuan27@gmail.com>
2020-12-25 00:31:03 +0800

    Mongoose part thress

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const Dishes = require('./modles/dishes');
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
 
+
+
 connect.then((db) =>{
     console.log('Connected correctly to server');
 
@@ -12,13 +14,27 @@ connect.then((db) =>{
     })
     .then((dish) => {
         console.log(dish);
-        return Dishes.find({});
-    })
-    .then((dishes) =>{
-        console.log(dishes);
 
-        return Dishes.remove({});
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set:{description: 'Updated test'},
+        },
+        { new: true
+        }).exec();
     })
+    .then((dish) => {
+        console.log(dish);
+dish.comments.push({
+        rating: 5,
+        comment: 'I\'m getting a sinking feeling!',
+        author: 'Leonardo di Carpaccio'
+    });
+
+    return dish.save();
+    })
+    .then((dish) =>{
+        console.log(dish);
+    })
+    
     .then(() =>{
         return mongoose.connection.close();
     })
--- modles/dishes.js
+++ modles/dishes.js
@@ -1,6 +1,24 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        require: true
+
+    },
+    comment:{
+        type: String,
+        required: true
+    },
+    author:{
+        type: String,
+        required: true
+    }
+});
+
 const dishSchema = new Schema({
     name: {
         type: String,
@@ -10,7 +28,8 @@ const dishSchema = new Schema({
     description: {
         type: String,
         required:true
-    }
+    },
+    comments: [commentSchema]
 },   {
         timestamps: true
 });
